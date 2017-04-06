41da08c0e9dae30556520025cc04a7c2aca4496d
Josh Harwood <joshuamharwood@gmail.com>
2017-04-06 18:08:13 +0100

    parse search result per recipe

--- scrapers/bbcgoodfood/index.js
+++ scrapers/bbcgoodfood/index.js
@@ -8,7 +8,10 @@ request(baseUrl, (err, res, body) => {
   if(err) return console.error(err);
 
   let $ = cheerio.load(body);
-  $("article.node-recipe").each(() => {
-    console.log("recipe");
+  $("article.node-recipe").each(function(i, e) {
+    let recipe = $(this).children("h3.teaser-item__title").first();
+    let url = recipe.children().first().attr("href");
+    console.log(recipe.text().trim());
+    console.log(url);
   });
 });
