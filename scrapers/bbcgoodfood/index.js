af91877be155be7cbfb9ccb7ae607427c46dd1ae
Josh Harwood <joshuamharwood@gmail.com>
2017-04-06 18:55:28 +0100

    added iterating over multiple result pages

--- scrapers/bbcgoodfood/index.js
+++ scrapers/bbcgoodfood/index.js
@@ -2,16 +2,46 @@ const fs = require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
-baseUrl = "https://www.bbcgoodfood.com/search?query=";
+function parseNextPage($, callback){
+  let url = $("ul.pager").children().last().children().first().attr("href");
+  callback(url);
+}
 
-request(baseUrl, (err, res, body) => {
-  if(err) return console.error(err);
-
-  let $ = cheerio.load(body);
+function parseResultPage($){
+  /* find all recipes in result page */
   $("article.node-recipe").each(function(i, e) {
     let recipe = $(this).children("h3.teaser-item__title").first();
     let url = recipe.children().first().attr("href");
-    console.log(recipe.text().trim());
-    console.log(url);
+    parseRecipe(url);
   });
-});
+}
+
+function parseRecipe(url){
+  console.log(url);
+}
+
+function visitPage(url){
+  request(url, (err, res, body) => {
+    if(err) return console.error(err);
+
+    let $ = cheerio.load(body);
+
+    /* get all recipes for that page of results */
+    parseResultPage($);
+
+    /* parse 'nextpage' button and visit it */
+    parseNextPage($, (nextUrl) => {
+      if(nextUrl != undefined){
+        visitPage(baseUrl + nextUrl)
+      }
+    });
+  });
+}
+
+baseUrl = "https://www.bbcgoodfood.com";
+
+/* Parse first page */
+let finished = false;
+let url = baseUrl + "/search?query=";
+
+visitPage(url);
