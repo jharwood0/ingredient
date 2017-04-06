ab282f8eecb8fb43516626d2250fa9a5fca17acd
Josh Harwood <joshuamharwood@gmail.com>
2017-04-06 21:47:25 +0100

    added half of the recipe scrape

--- scrapers/bbcgoodfood/index.js
+++ scrapers/bbcgoodfood/index.js
@@ -5,7 +5,40 @@ const cheerio = require('cheerio');
 const baseUrl = "https://www.bbcgoodfood.com";
 
 function parseRecipe(url){
-  console.log(url);
+  request(baseUrl + url, function(err, res, body) {
+    if(err) return console.error(err);
+
+    let $ = cheerio.load(body);
+
+    /* Needed:
+    *   name
+    *   ingredients: ["",""]
+    *   nutrition: {"kcal, fat, saturates, carbs, sugars, fibre, protein, salt"}
+    *   prep_time
+    *   cook_time
+    *   skill
+    *   serving
+    *   steps: ["",""]
+    */
+    let recipe = {};
+    /* name */
+    recipe.name = $("h1.recipe-header__title").text().trim();
+    /* prep time */
+    recipe.prep_time = "";
+    $("span.recipe-details__cooking-time-prep").each(function(){
+      recipe.prep_time += ($(this).text());
+    });
+    recipe.prep_time = recipe.prep_time.replace("Prep: ", "").trim();
+    /* cook time */
+    recipe.cook_time = "";
+    $("span.recipe-details__cooking-time-cook").each(function(){
+      recipe.cook_time += ($(this).text());
+    });
+    recipe.cook_time = recipe.cook_time.replace("Cook: ", "").trim();
+    /* skill */
+    recipe.skill = $("section.recipe-details__item recipe-details__item--skill-level").find("span.recipe-details__text").text();
+    console.log(recipe);
+  });
 }
 
 function visitPage(url){
